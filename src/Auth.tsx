a3483e2ab265648b8c676e92d7ca56f1e7801e71
Aloento <11802769+Aloento@users.noreply.github.com>
2024-08-05 15:48:00 +0200

    feat: Add OIDCProvider component for authentication
    The OIDCProvider component is added to provide authentication functionality using OpenID Connect. This component wraps the application with the necessary authentication context and handles the authentication flow.

---
+++ src/Auth.tsx
@@ -0,0 +1,76 @@
+import { useMount, useUpdateEffect } from "ahooks";
+import { Logger, WebStorageStateStore } from "oidc-client-ts";
+import { ReactNode } from "react";
+import { AuthProvider, hasAuthParams, useAuth } from "react-oidc-context";
+
+/**
+ * @author Aloento
+ * @since 1.0.0
+ * @version 0.1.0
+ */
+export function OIDCProvider({ children }: { children: ReactNode }): ReactNode {
+  const { Rep } = useRouter();
+
+  return (
+    <AuthProvider
+      client_id="status-dashboard"
+      scope="openid profile email"
+      userStore={new WebStorageStateStore({ store: window.localStorage })}
+      onSigninCallback={() => {
+        Rep("/");
+        location.reload();
+      }}
+      authority={
+        process.env.NODE_ENV === "development"
+          ? "http://80.158.108.251:8080/"
+          : "https://keycloak.eco.tsi-dev.otc-service.com/realms/eco"
+      }
+      post_logout_redirect_uri={
+        process.env.NODE_ENV === "development"
+          ? "http://localhost:9000/Logout"
+          : "https://sd3.eco.tsi-dev.otc-service.com/Logout"
+      }
+      redirect_uri={
+        process.env.NODE_ENV === "development"
+          ? "http://localhost:9000/Login"
+          : "https://sd3.eco.tsi-dev.otc-service.com/Login"
+      }
+    >
+      <AuthHandler />
+      {children}
+    </AuthProvider>
+  );
+}
+
+const log = new Logger("Auth");
+
+/**
+ * @author Aloento
+ * @since 1.0.0
+ * @version 0.1.0
+ */
+function AuthHandler() {
+  const auth = (Common.AuthSlot = useAuth());
+  const { Paths, Rep } = useRouter();
+
+  useMount(() => {
+    if (Paths.at(0) === "Logout") {
+      auth.removeUser();
+      return Rep("/");
+    }
+
+    if (
+      !hasAuthParams() &&
+      !auth.isAuthenticated &&
+      !auth.activeNavigator &&
+      !auth.isLoading
+    )
+      auth.signinRedirect();
+  });
+
+  useUpdateEffect(() => {
+    if (auth.error) log.warn(auth.error);
+  }, [auth.error]);
+
+  return null;
+}

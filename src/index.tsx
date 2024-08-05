1c0e60b2e86533de4f18f935fd030364fa4b0fd3
Aloento <11802769+Aloento@users.noreply.github.com>
2024-08-05 15:48:05 +0200

    chore: Update body tag with data-mode attribute

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import "@telekom/scale-components/dist/scale-components/scale-components.css";
 import "./index.css";
 
+import { defineCustomElements } from "@telekom/scale-components/loader";
 import { createRoot } from "react-dom/client";
-import { Main } from "./main";
+
+defineCustomElements();
 
 const container = document.querySelector("#root")!;
 const root = createRoot(container);
 
-root.render(<Main />);
+root.render(
+  <div>
+
+  </div>
+);

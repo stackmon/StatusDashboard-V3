883c9861b4d1af9e099b1b391ad3a06d9707596b
Aloento <11802769+Aloento@users.noreply.github.com>
2024-08-05 14:17:14 +0200

    chore: Update package dependencies and add Telekom Scale Components

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
-import { createRoot } from "react-dom/client";
+import "@telekom/scale-components/dist/scale-components/scale-components.css";
 import "./index.css";
+
+import { createRoot } from "react-dom/client";
 import { Main } from "./main";
 
 const container = document.querySelector("#root")!;

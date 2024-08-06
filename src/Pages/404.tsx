2c5d0ea8c1b075998f9c4d897873d4171aeea025
Aloento <11802769+Aloento@users.noreply.github.com>
2024-08-06 15:42:34 +0200

    feat: Add 404 page for handling not found routes

---
+++ src/Pages/404.tsx
@@ -0,0 +1,23 @@
+import { ScaleLoadingSpinner } from "@telekom/scale-components-react";
+import { Helmet } from "react-helmet";
+import { useRouter } from "~/Components/Router";
+import { Dic } from "~/Helpers/Entities";
+
+/**
+ * @author Aloento
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+export function NotFound() {
+  const { Rep } = useRouter();
+  setTimeout(() => Rep("/"), 3000);
+
+  return <>
+    <Helmet>
+      <title>Redirect - Not Found - {Dic.Name}</title>
+      <meta name="robots" content="noindex, nofollow" />
+    </Helmet>
+
+    <ScaleLoadingSpinner size="large" text="Redirecting..." />
+  </>;
+}

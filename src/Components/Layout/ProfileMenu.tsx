f5ce0d62b79809aff880e20107312d42cdfb6eeb
Aloento <11802769+Aloento@users.noreply.github.com>
2024-08-08 15:08:23 +0200

    refactor: Update ProfileMenu component to use ScaleButton and ScaleMenuFlyout
    The ProfileMenu component is refactored to use the ScaleButton and ScaleMenuFlyout components from the @telekom/scale-components-react package. This improves the overall design and functionality of the profile menu by providing a more consistent and user-friendly experience.

--- src/Components/Layout/ProfileMenu.tsx
+++ src/Components/Layout/ProfileMenu.tsx
@@ -1,9 +1,48 @@
-import { ScaleTelekomNavItem } from "@telekom/scale-components-react";
+import { ScaleButton, ScaleIconUserFileUser, ScaleMenuFlyout, ScaleMenuFlyoutItem, ScaleMenuFlyoutList, ScaleTelekomNavItem } from "@telekom/scale-components-react";
+import { useAuth } from "react-oidc-context";
+import { Authorized, NotAuthorized } from "../Auth/With";
 
+/**
+ * @author Aloento
+ * @since 1.0.0
+ * @version 0.1.0
+ */
 export function ProfileMenu() {
+  const auth = useAuth();
+
   return (
     <ScaleTelekomNavItem hideOnMobile>
+      <ScaleMenuFlyout>
 
+        <ScaleButton slot="trigger" variant="secondary" iconOnly>
+          <ScaleIconUserFileUser accessibility-title="Menu" />
+        </ScaleButton>
+
+        <ScaleMenuFlyoutList>
+          <Authorized>
+            <ScaleMenuFlyoutItem>
+              Hi, {auth.user?.profile.name}
+            </ScaleMenuFlyoutItem>
+
+            <ScaleMenuFlyoutItem onClick={() => auth.signoutRedirect()}>
+              Logout
+            </ScaleMenuFlyoutItem>
+
+            <ScaleMenuFlyoutItem>
+              <a className="text-black no-underline" href="/NewEvent">
+                New Event
+              </a>
+            </ScaleMenuFlyoutItem>
+          </Authorized>
+
+          <NotAuthorized>
+            <ScaleMenuFlyoutItem onClick={() => auth.signinRedirect()}>
+              Login
+            </ScaleMenuFlyoutItem>
+          </NotAuthorized>
+        </ScaleMenuFlyoutList>
+
+      </ScaleMenuFlyout>
     </ScaleTelekomNavItem>
   );
 }

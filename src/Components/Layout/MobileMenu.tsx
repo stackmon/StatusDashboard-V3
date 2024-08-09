0862dea5b7a9886994c4f8b108e948507f162e46
Aloento <11802769+Aloento@users.noreply.github.com>
2024-08-09 15:08:09 +0200

    feat: Update MobileMenu component with Scale components and authentication logic
    The MobileMenu component is updated to use the Scale components from the @telekom/scale-components-react package. Additionally, authentication logic is added using the Authorized and NotAuthorized components. This improves the design and functionality of the mobile menu by providing a more consistent and user-friendly experience.

--- src/Components/Layout/MobileMenu.tsx
+++ src/Components/Layout/MobileMenu.tsx
@@ -1,9 +1,58 @@
-import { ScaleTelekomNavItem } from "@telekom/scale-components-react";
+import { ScaleIconActionMenu, ScaleTelekomMobileFlyoutCanvas, ScaleTelekomMobileMenu, ScaleTelekomMobileMenuItem, ScaleTelekomNavFlyout, ScaleTelekomNavItem } from "@telekom/scale-components-react";
+import { useAuth } from "react-oidc-context";
+import { Authorized, NotAuthorized } from "../Auth/With";
 
+/**
+ * @author Aloento
+ * @since 1.0.0
+ * @version 0.1.0
+ */
 export function MobileMenu() {
+  const auth = useAuth();
+
   return (
     <ScaleTelekomNavItem hideOnDesktop>
+      <button>
+        <ScaleIconActionMenu accessibility-title="Menu" />
+      </button>
 
+      <ScaleTelekomNavFlyout>
+        <ScaleTelekomMobileFlyoutCanvas>
+          <ScaleTelekomMobileMenu slot="mobile-main-nav">
+
+            <ScaleTelekomMobileMenuItem>
+              <a href="/History">History</a>
+            </ScaleTelekomMobileMenuItem>
+
+            <ScaleTelekomMobileMenuItem>
+              <a href="/Availability">Availability</a>
+            </ScaleTelekomMobileMenuItem>
+
+            <ScaleTelekomMobileMenuItem>
+              <a href="https://docs.otc.t-systems.com/status-dashboard/index.html" target="_black">
+                Docs
+              </a>
+            </ScaleTelekomMobileMenuItem>
+
+            <Authorized>
+              <ScaleTelekomMobileMenuItem>
+                <a href="/NewEvent">New Event</a>
+              </ScaleTelekomMobileMenuItem>
+
+              <ScaleTelekomMobileMenuItem onClick={() => auth.signoutRedirect()}>
+                Logout {auth.user?.profile.name}
+              </ScaleTelekomMobileMenuItem>
+            </Authorized>
+
+            <NotAuthorized>
+              <ScaleTelekomMobileMenuItem onClick={() => auth.signinRedirect()}>
+                Login
+              </ScaleTelekomMobileMenuItem>
+            </NotAuthorized>
+
+          </ScaleTelekomMobileMenu>
+        </ScaleTelekomMobileFlyoutCanvas>
+      </ScaleTelekomNavFlyout>
     </ScaleTelekomNavItem>
   );
 }

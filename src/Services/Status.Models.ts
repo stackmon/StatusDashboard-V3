0cee54ad922192406e56a2ff3eefd4109beab1a2
Aloento <11802769+Aloento@users.noreply.github.com>
2024-08-11 04:39:36 +0200

    feat: Add IStatusContext interface and Models namespace
    The `IStatusContext` interface is added to the `Status.Models.ts` file to define the status context for the application. The `Models` namespace is also added to encapsulate the various models used in the status module. This improves the organization and maintainability of the codebase.

---
+++ src/Services/Status.Models.ts
@@ -0,0 +1,62 @@
+import { Dayjs } from "dayjs";
+import { EventStatus, EventType } from "~/Components/Event/Enums";
+
+/**
+ * @author Aloento
+ * @since 1.0.0
+ * @version 0.1.0
+ */
+export interface IStatusContext {
+
+}
+
+/**
+ * @author Aloento
+ * @since 1.0.0
+ * @version 0.1.0
+ */
+export namespace Models {
+  export interface IService {
+    Id: number;
+    Name: string;
+    Abbr: string;
+    Category: ICategory;
+    Regions: IRegion[];
+  }
+
+  export interface ICategory {
+    Id: number;
+    Name: string;
+    Services: IService[];
+  }
+
+  export interface IRegion {
+    Id: number;
+    Name: string;
+    Services: IService[];
+  }
+
+  export interface IRegionService {
+    Region: IRegion;
+    Service: IService;
+    Events: IEvent[];
+  }
+
+  export interface IEvent {
+    Id: number;
+    Title: string;
+    Type: EventType;
+    Start: Dayjs;
+    End: Dayjs;
+    RegionServices: IRegionService[];
+    Histories: IHistory[];
+  }
+
+  export interface IHistory {
+    Id: number;
+    Message: string;
+    Created: Dayjs;
+    Status: EventStatus;
+    Event: IEvent;
+  }
+}

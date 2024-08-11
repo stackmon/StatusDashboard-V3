2b2cc887ec19071a8bb436368471f1a8225b62c8
Aloento <11802769+Aloento@users.noreply.github.com>
2024-08-11 09:04:16 +0200

    feat: Add Status.Entities.ts for defining status-related interfaces

---
+++ src/Services/Status.Entities.ts
@@ -0,0 +1,52 @@
+/**
+ * @author Aloento
+ * @since 1.0.0
+ * @version 0.1.0
+ */
+export interface IStatusEntity {
+  attributes: AttributeEntity[];
+  id: number;
+  incidents: IncidentEntity[];
+  name: string;
+}
+
+interface AttributeEntity {
+  name: NameEnum;
+  value: string;
+}
+
+const enum NameEnum {
+  Category = "category",
+  Region = "region",
+  Type = "type",
+}
+
+interface IncidentEntity {
+  end_date: null | string;
+  id: number;
+  impact: number;
+  start_date: string;
+  text: string;
+  updates: UpdateEntity[];
+}
+
+interface UpdateEntity {
+  status: StatusEnum;
+  text: string;
+  timestamp: string;
+}
+
+const enum StatusEnum {
+  Analyzing = "analyzing",
+  Changed = "changed",
+  Completed = "completed",
+  Description = "description",
+  Fixing = "fixing",
+  InProgress = "in progress",
+  Modified = "modified",
+  Observing = "observing",
+  Reopened = "reopened",
+  Resolved = "resolved",
+  Scheduled = "scheduled",
+  System = "SYSTEM",
+}

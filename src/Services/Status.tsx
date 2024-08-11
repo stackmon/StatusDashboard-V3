c41305c4864e4ac950da16a9912a21cdfbe29de5
Aloento <11802769+Aloento@users.noreply.github.com>
2024-08-11 09:04:19 +0200

    feat: Add lazy loading for images in Status.tsx

--- src/Services/Status.tsx
+++ src/Services/Status.tsx
@@ -1,3 +1,4 @@
+import { useRequest } from "ahooks";
 import { createContext, useState } from "react";
 import { Logger } from "~/Helpers/Logger";
 import { IStatusContext } from "./Status.Models";
@@ -32,6 +33,19 @@ const log = new Logger("Service", "Status");
  */
 export function StatusContext({ children }: { children: JSX.Element }) {
   const [db, setDB] = useState(DB);
+
+  useRequest(async () => {
+    log.info("Loading status data...");
+    const response = await fetch("https://status.cloudmon.eco.tsi-dev.otc-service.com/api/v1/component_status");
+    const data = await response.json();
+    log.debug("Status data loaded.", data);
+    return data;
+  }, {
+    cacheKey: log.namespace,
+    onSuccess: (data) => {
+
+    }
+  });
 
   function update(data: IStatusContext) {
     DB = { ...data };

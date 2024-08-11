5e4a131a9dd16a331cd5434daaa584c5945255e7
Aloento <11802769+Aloento@users.noreply.github.com>
2024-08-11 05:22:45 +0200

    feat: Add StatusContext component for managing status-related data

---
+++ src/Services/Status.tsx
@@ -0,0 +1,46 @@
+import { createContext, useState } from "react";
+import { Logger } from "~/Helpers/Logger";
+import { IStatusContext } from "./Status.Models";
+
+/**
+ * @author Aloento
+ * @since 1.0.0
+ * @version 0.1.0
+ */
+export let DB: IStatusContext = {
+  Services: [],
+  Categories: [],
+  Regions: [],
+  Events: [],
+  Histories: [],
+  RegionsServices: []
+}
+
+interface IContext {
+  DB: IStatusContext;
+  Update: (data: IStatusContext) => void;
+}
+
+const CTX = createContext<IContext>({} as IContext);
+
+const log = new Logger("Service", "Status");
+
+/**
+ * @author Aloento
+ * @since 1.0.0
+ * @version 0.1.0
+ */
+export function StatusContext({ children }: { children: JSX.Element }) {
+  const [db, setDB] = useState(DB);
+
+  function update(data: IStatusContext) {
+    DB = { ...data };
+    setDB(DB);
+  }
+
+  return (
+    <CTX.Provider value={{ DB: db, Update: update }}>
+      {children}
+    </CTX.Provider>
+  );
+}

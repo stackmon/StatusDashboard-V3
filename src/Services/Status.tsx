73cad7e2115789d28f4f4314b87bd75b6f7a3449
Aloento <11802769+Aloento@users.noreply.github.com>
2024-08-12 04:57:16 +0200

    feat: Refactor Status.tsx to improve data loading and organization

--- src/Services/Status.tsx
+++ src/Services/Status.tsx
@@ -1,6 +1,7 @@
 import { useRequest } from "ahooks";
 import { createContext, useState } from "react";
 import { Logger } from "~/Helpers/Logger";
+import { NameEnum, StatusEntity } from "./Status.Entities";
 import { IStatusContext } from "./Status.Models";
 
 /**
@@ -14,8 +15,8 @@ export let DB: IStatusContext = {
   Regions: [],
   Events: [],
   Histories: [],
-  RegionsServices: []
-}
+  RegionService: [],
+};
 
 interface IContext {
   DB: IStatusContext;
@@ -25,6 +26,7 @@ interface IContext {
 const CTX = createContext<IContext>({} as IContext);
 
 const log = new Logger("Service", "Status");
+let id = 0;
 
 /**
  * @author Aloento
@@ -34,18 +36,95 @@ const log = new Logger("Service", "Status");
 export function StatusContext({ children }: { children: JSX.Element }) {
   const [db, setDB] = useState(DB);
 
-  useRequest(async () => {
-    log.info("Loading status data...");
-    const response = await fetch("https://status.cloudmon.eco.tsi-dev.otc-service.com/api/v1/component_status");
-    const data = await response.json();
-    log.debug("Status data loaded.", data);
-    return data;
-  }, {
-    cacheKey: log.namespace,
-    onSuccess: (data) => {
+  useRequest(
+    async () => {
+      log.info("Loading status data...");
+      const response = await fetch(
+        "https://status.cloudmon.eco.tsi-dev.otc-service.com/api/v1/component_status"
+      );
+      const data = await response.json();
+      log.debug("Status data loaded.", data);
+      return data as StatusEntity[];
+    },
+    {
+      cacheKey: log.namespace,
+      onSuccess: (list) => {
+        for (const item of list) {
+          if (item.attributes.length < 3) {
+            log.debug("Skipped Hidden Item.", item);
+            continue;
+          }
 
+          const targetCate = item.attributes.find(
+            (x) => x.name === NameEnum.Category
+          )?.value;
+          if (!targetCate) {
+            log.debug("Skipped Null Category.", item);
+            continue;
+          }
+
+          let dbCate = db.Categories.find((x) => x.Name === targetCate);
+          if (!dbCate) {
+            dbCate = { Id: id++, Name: targetCate, Services: new Set() };
+            db.Categories.push(dbCate);
+          }
+
+          const targetRegion = item.attributes.find(
+            (x) => x.name === NameEnum.Region
+          )?.value;
+          if (!targetRegion) {
+            log.debug("Skipped Null Region.", item);
+            continue;
+          }
+
+          let dbRegion = db.Regions.find((x) => x.Name === targetRegion);
+          if (!dbRegion) {
+            dbRegion = { Id: id++, Name: targetRegion, Services: new Set() };
+            db.Regions.push(dbRegion);
+          }
+
+          const targetService = item.name;
+
+          let dbService = db.Services.find((x) => x.Name === targetService);
+          if (!dbService) {
+            const abbr = item.attributes.find((x) => x.name === NameEnum.Type)?.value;
+            if (!abbr) {
+              log.debug("Skipped Null Abbr.", item);
+              continue;
+            }
+
+            dbService = {
+              Id: id++,
+              Name: targetService,
+              Abbr: abbr,
+              Category: dbCate,
+              Regions: new Set([dbRegion]),
+            };
+            db.Services.push(dbService);
+          }
+
+          dbService.Regions.add(dbRegion);
+          dbRegion.Services.add(dbService);
+          dbCate.Services.add(dbService);
+
+          const regionService = db.RegionService.find(
+            (x) => x.Region === dbRegion && x.Service === dbService
+          );
+          if (!regionService) {
+            db.RegionService.push({
+              Region: dbRegion,
+              Service: dbService,
+              Events: new Set(),
+            });
+          }
+
+          for (const incident of item.incidents) {
+            const dbEvent = db.Events.find((x) => x.Id === incident.id);
+          }
+        }
+      },
     }
-  });
+  );
 
   function update(data: IStatusContext) {
     DB = { ...data };
@@ -53,8 +132,6 @@ export function StatusContext({ children }: { children: JSX.Element }) {
   }
 
   return (
-    <CTX.Provider value={{ DB: db, Update: update }}>
-      {children}
-    </CTX.Provider>
+    <CTX.Provider value={{ DB: db, Update: update }}>{children}</CTX.Provider>
   );
 }

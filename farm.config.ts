24a20cf5467680f73126b15fc0aeb50f9d7e3130
Aloento <11802769+Aloento@users.noreply.github.com>
2024-08-15 16:18:42 +0200

    feat: Add compilation and server configurations to farm.config.ts
    The farm.config.ts file is updated to include compilation and server configurations. The compilation configuration adds a resolve alias for the "~/src" directory, allowing for easier import paths. The server configuration sets up a proxy for the "/api" route, redirecting requests to "https://status.cloudmon.eco.tsi-dev.otc-service.com". These changes improve the configuration and functionality of the application.

--- farm.config.ts
+++ farm.config.ts
@@ -1,6 +1,22 @@
 import { defineConfig } from "@farmfe/core";
 import farmPostcssPlugin from "@farmfe/js-plugin-postcss";
+import path from "path";
 
 export default defineConfig({
   plugins: ["@farmfe/plugin-react", farmPostcssPlugin()],
+  compilation: {
+    resolve: {
+      alias: {
+        "~/": path.join(process.cwd(), "src"),
+      }
+    }
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://status.cloudmon.eco.tsi-dev.otc-service.com",
+        changeOrigin: true
+      }
+    }
+  }
 });

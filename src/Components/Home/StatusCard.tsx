d902b1c3dc6df19331b5faa6f6c44b5e8e8c86db
Aloento <11802769+Aloento@users.noreply.github.com>
2024-08-25 15:45:02 +0200

    feat: Add StatusCard component for displaying status information
    A new component called StatusCard is added to display status information. This component retrieves data from the Status service and renders the relevant information. It enhances the functionality and user experience of the application.

---
+++ src/Components/Home/StatusCard.tsx
@@ -0,0 +1,55 @@
+import { ScaleDivider } from "@telekom/scale-components-react";
+import { useCreation } from "ahooks";
+import { chain } from "lodash";
+import { useEffect, useMemo, useState } from "react";
+import { BehaviorSubject } from "rxjs";
+import { Station } from "~/Helpers/Entities";
+import { useStatus } from "~/Services/Status";
+import { Models } from "~/Services/Status.Models";
+import { ServiceItem } from "./ServiceItem";
+
+interface IStatusCard {
+  Category: Models.ICategory;
+}
+
+/**
+ * @author Aloento
+ * @since 1.0.0
+ * @version 0.1.0
+ */
+export function StatusCard({ Category }: IStatusCard) {
+  const { DB } = useStatus();
+  const [region, setRegion] = useState(DB.Regions[0]);
+
+  const regionSub = useCreation(
+    () => Station.get<BehaviorSubject<Models.IRegion>>("HomeRegion"), []);
+
+  useEffect(() => {
+    const sub = regionSub.subscribe(setRegion);
+    return () => sub.unsubscribe();
+  }, []);
+
+  const services = useMemo(() =>
+    chain(DB.RegionService)
+      .filter(x => x.Region === region)
+      .filter(x => x.Service.Category === Category)
+      .orderBy(x => x.Service.Name)
+      .value()
+    , [DB, region, Category]);
+
+  return (
+    <div className="flex h-fit flex-col rounded-md bg-white px-3.5 py-5 shadow-md">
+      <h3 className="px-3.5 text-2xl font-medium text-slate-800">
+        {Category.Name}
+      </h3>
+
+      <ScaleDivider className="h-auto" />
+
+      <ul className="px-3.5">
+        {services.map(item => (
+          <ServiceItem RegionService={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}

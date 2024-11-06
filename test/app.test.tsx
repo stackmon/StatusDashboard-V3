bbc4d551e22a8d6f8841d19a7cbe7cf18bfd63a8
Aloento <11802769+Aloento@users.noreply.github.com>
2024-11-06 12:43:34 +0100

    feat: Update tests to improve rendering checks and add Home component tests

--- test/app.test.tsx
+++ test/app.test.tsx
@@ -1,10 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import { App } from "~/App";
 
 describe("App", () => {
-  it("it should be render", () => {
-    render(<App />);
-    expect(screen.getByText("Imprint")).toBeInTheDocument();
+  it("should render skeleton", () => {
+    const { getByText } = render(<App />);
+    expect(getByText("Imprint")).toBeInTheDocument();
   });
 });

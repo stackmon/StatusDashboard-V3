4cd8e9a350e3bfd0ca2307655bbaa120d1bdb9cf
Aloento <11802769+Aloento@users.noreply.github.com>
2024-11-27 16:45:57 +0100

    [refactor] Improve environment variable handling in farm.config.ts

--- farm.config.ts
+++ farm.config.ts
@@ -2,38 +2,44 @@ import { defineConfig } from "@farmfe/core";
 import farmPostcssPlugin from "@farmfe/js-plugin-postcss";
 import path from "path";
 
-const env = {
-  SD_BACKEND_URL: process.env.SD_BACKEND_URL,
-  SD_CLIENT_ID: process.env.SD_CLIENT_ID,
-  SD_AUTHORITY_URL: process.env.SD_AUTHORITY_URL,
-  SD_REDIRECT_URL: process.env.SD_REDIRECT_URL,
-  SD_LOGOUT_REDIRECT_URL: process.env.SD_LOGOUT_REDIRECT_URL,
-  SD_AUTH_SECRET: process.env.SD_AUTH_SECRET,
-};
+export default defineConfig((cfg) => {
+  let env: object | undefined;
 
-for (const [key, value] of Object.entries(env)) {
-  if (!value) {
-    throw new Error(`Environment variable ${key} is missing or empty.`);
-  }
-}
+  if (cfg.mode !== "development") {
+    env = {
+      SD_BACKEND_URL: process.env.SD_BACKEND_URL,
+      SD_CLIENT_ID: process.env.SD_CLIENT_ID,
+      SD_AUTHORITY_URL: process.env.SD_AUTHORITY_URL,
+      SD_REDIRECT_URL: process.env.SD_REDIRECT_URL,
+      SD_LOGOUT_REDIRECT_URL: process.env.SD_LOGOUT_REDIRECT_URL,
+      SD_AUTH_SECRET: process.env.SD_AUTH_SECRET,
+    };
 
-export default defineConfig({
-  plugins: ["@farmfe/plugin-react", farmPostcssPlugin()],
-  compilation: {
-    resolve: {
-      alias: {
-        "~/": path.join(process.cwd(), "src"),
-      }
-    },
-    define: env,
-  },
-  envPrefix: "SD_",
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://status.cloudmon.eco.tsi-dev.otc-service.com",
-        changeOrigin: true
+    for (const [key, value] of Object.entries(env)) {
+      if (!value) {
+        throw new Error(`Environment variable ${key} is missing or empty.`);
       }
     }
   }
+
+  return {
+    plugins: ["@farmfe/plugin-react", farmPostcssPlugin()],
+    compilation: {
+      resolve: {
+        alias: {
+          "~/": path.join(process.cwd(), "src"),
+        }
+      },
+      define: env,
+    },
+    envPrefix: "SD_",
+    server: {
+      proxy: {
+        "/api": {
+          target: "https://status.cloudmon.eco.tsi-dev.otc-service.com",
+          changeOrigin: true
+        }
+      }
+    }
+  };
 });

b5b42ef4ae0094a09cfa2c2fa6da3d6df6d06347
Aloento <11802769+Aloento@users.noreply.github.com>
2024-11-28 10:53:40 +0100

    [fix] Getting the system environment (#31)

--- farm.config.ts
+++ farm.config.ts
@@ -2,30 +2,43 @@ import { defineConfig } from "@farmfe/core";
 import farmPostcssPlugin from "@farmfe/js-plugin-postcss";
 import path from "path";
 
-export default defineConfig({
-  plugins: ["@farmfe/plugin-react", farmPostcssPlugin()],
-  compilation: {
-    resolve: {
-      alias: {
-        "~/": path.join(process.cwd(), "src"),
-      }
-    },
-    define: {
+export default defineConfig((cfg) => {
+  let env: object | undefined;
+
+  if (cfg.mode !== "development") {
+    env = {
       SD_BACKEND_URL: process.env.SD_BACKEND_URL,
       SD_CLIENT_ID: process.env.SD_CLIENT_ID,
       SD_AUTHORITY_URL: process.env.SD_AUTHORITY_URL,
       SD_REDIRECT_URL: process.env.SD_REDIRECT_URL,
-      SD_LOGOUT_REDIRECT_URL: process.env.SD_LOGOUT_REDIRECT_URL,
-      SD_AUTH_SECRET: process.env.SD_AUTH_SECRET,
-    }
-  },
-  envPrefix: "SD_",
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://status.cloudmon.eco.tsi-dev.otc-service.com",
-        changeOrigin: true
+      SD_LOGOUT_REDIRECT_URL: process.env.SD_LOGOUT_REDIRECT_URL
+    };
+
+    for (const [key, value] of Object.entries(env)) {
+      if (!value) {
+        throw new Error(`Environment variable ${key} is missing or empty.`);
       }
     }
   }
+
+  return {
+    plugins: ["@farmfe/plugin-react", farmPostcssPlugin()],
+    compilation: {
+      resolve: {
+        alias: {
+          "~/": path.join(process.cwd(), "src"),
+        }
+      },
+      define: env,
+    },
+    envPrefix: "SD_",
+    server: {
+      proxy: {
+        "/api": {
+          target: "https://status.cloudmon.eco.tsi-dev.otc-service.com",
+          changeOrigin: true
+        }
+      }
+    }
+  };
 });

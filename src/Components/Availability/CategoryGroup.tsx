11072266e6f137acb3adfbc135c93c9e58f877b1
Aloento <11802769+Aloento@users.noreply.github.com>
2024-12-01 12:15:43 +0100

    [refactor] Replace service retrieval logic in CategoryGroup with useAvailability hook

--- src/Components/Availability/CategoryGroup.tsx
+++ src/Components/Availability/CategoryGroup.tsx
@@ -1,11 +1,5 @@
-import { useCreation } from "ahooks";
-import { chain } from "lodash";
-import { useEffect, useMemo, useState } from "react";
-import { BehaviorSubject } from "rxjs";
-import { Station } from "~/Helpers/Entities";
-import { Calc6Months } from "~/Helpers/SLA";
-import { useStatus } from "~/Services/Status";
 import { Models } from "~/Services/Status.Models";
+import { useAvailability } from "./useAvailability";
 
 interface ICategoryGroup {
   Category: Models.ICategory;
@@ -18,30 +12,7 @@ interface ICategoryGroup {
  * @version 0.1.0
  */
 export function CategoryGroup({ Category, Topic }: ICategoryGroup) {
-  const { DB } = useStatus();
-
-  const [region, setRegion] = useState(DB.Regions[0]);
-  const regionSub = useCreation(
-    () => Station.get<BehaviorSubject<Models.IRegion>>(Topic), []);
-
-  useEffect(() => {
-    const sub = regionSub.subscribe(setRegion);
-    return () => sub.unsubscribe();
-  }, []);
-
-  const [services, slas] = useMemo(() => {
-    const s = chain(DB.RegionService)
-      .filter(x => x.Region.Id === region.Id)
-      .filter(x => x.Service.Category.Id === Category.Id)
-      .orderBy(x => x.Service.Name)
-      .value();
-
-    const a = chain(s)
-      .map(x => Calc6Months(x))
-      .value();
-
-    return [s, a];
-  }, [DB, region, Category]);
+  const avas = useAvailability(Category, Topic);
 
   function getColor(val: number): string {
     const color = val >= 99.95
@@ -53,30 +24,30 @@ export function CategoryGroup({ Category, Topic }: ICategoryGroup) {
 
   return <>
     <tr>
-      <td rowSpan={services.length} className="text-lg">
+      <td rowSpan={avas.length} className="text-lg">
         {Category.Name}
       </td>
 
       <td className="text-lg">
-        {services[0].Service.Name}
+        {avas[0].RS.Service.Name}
       </td>
 
-      {slas[0].map((sla, i) => (
-        <td key={i} className={`border-l text-center ${getColor(sla)}`}>
-          {sla.toFixed(2)}
+      {avas[0].Percentages.map((perc, i) => (
+        <td key={i} className={`border-l text-center ${getColor(perc)}`}>
+          {perc.toFixed(2)}
         </td>
       ))}
     </tr>
 
-    {services.slice(1).map((service, i) => (
+    {avas.slice(1).map((ava, i) => (
       <tr key={i}>
         <td className="text-lg">
-          {service.Service.Name}
+          {ava.RS.Service.Name}
         </td>
 
-        {slas[i + 1].map((sla, j) => (
-          <td key={j} className={`border-l text-center ${getColor(sla)}`}>
-            {sla.toFixed(2)}
+        {ava.Percentages.map((perc, j) => (
+          <td key={j} className={`border-l text-center ${getColor(perc)}`}>
+            {perc.toFixed(2)}
           </td>
         ))}
       </tr>

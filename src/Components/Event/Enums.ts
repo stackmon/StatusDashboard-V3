296b685b9d95a315f6b8ed04a4cd92731c5309b3
Aloento <11802769+Aloento@users.noreply.github.com>
2025-01-17 03:44:47 +0100

    [feat] Adds event type and impact utility functions
    Implements functions to retrieve event types based on impact values and vice versa.
    
    Enhances code maintainability and readability by centralizing event type logic.

--- src/Components/Event/Enums.ts
+++ src/Components/Event/Enums.ts
@@ -16,6 +16,42 @@ export enum EventType {
  * @since 1.0.0
  * @version 0.1.0
  */
+export function GetEventType(impact: number): EventType {
+  switch (impact) {
+    case 0:
+      return EventType.Maintenance;
+    case 1:
+      return EventType.MinorIssue;
+    case 2:
+      return EventType.MajorIssue;
+    default:
+      return EventType.Outage;
+  }
+}
+
+/**
+ * @author Aloento
+ * @since 1.0.0
+ * @version 0.1.0
+ */
+export function GetEventImpact(type: EventType): number {
+  switch (type) {
+    case EventType.Maintenance:
+      return 0;
+    case EventType.MinorIssue:
+      return 1;
+    case EventType.MajorIssue:
+      return 2;
+    default:
+      return 3;
+  }
+}
+
+/**
+ * @author Aloento
+ * @since 1.0.0
+ * @version 0.1.0
+ */
 export enum EventStatus {
   Investigating = "Investigating",
   Fixing = "Fixing",

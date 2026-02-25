0fc0031fd4cb0f229a5ad407907eeec9da3129d0
Aloento <11802769+Aloento@users.noreply.github.com>
2026-02-26 00:12:40 +0100

    feat: update footer content to reflect T-Systems International GmbH and correct service links

--- src/Components/Layout/PageFooter.tsx
+++ src/Components/Layout/PageFooter.tsx
@@ -9,19 +9,19 @@ export function PageFooter() {
   return (
     <ScaleTelekomFooter slot="footer" type="minimal" class="mt-auto pt-8" data-mode="dark">
       <ScaleTelekomFooterContent>
-        <span slot="notice"> © Deutsche Telekom AG </span>
+        <span slot="notice"> © T-Systems International GmbH </span>
         <ul slot="navigation">
           <li>
-            <a href="https://open-telekom-cloud.com/en/imprint">Imprint</a>
+            <a href="https://public.t-cloud.com/en/imprint">Imprint</a>
           </li>
           <li>
-            <a href="https://open-telekom-cloud.com/en/data-protection">Data privacy</a>
+            <a href="https://public.t-cloud.com/en/data-privacy">Data privacy</a>
           </li>
           <li>
-            <a href="https://open-telekom-cloud.com/en/disclaimer-of-liability">Disclaimer of liability</a>
+            <a href="https://public.t-cloud.com/en/disclaimer-of-liability">Disclaimer of liability</a>
           </li>
           <li>
-            <a href="https://open-telekom-cloud.com/en/contact">Contact</a>
+            <a href="https://public.t-cloud.com/en/contact">Contact</a>
           </li>
         </ul>
       </ScaleTelekomFooterContent>

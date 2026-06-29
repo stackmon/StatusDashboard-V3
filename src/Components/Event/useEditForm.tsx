873a1e6c7f72fc235ccdd4f539cde7514490b19b
Aloento <11802769+Aloento@users.noreply.github.com>
2026-06-29 12:00:08 +0200

    [Fix] Description is required for Maintenance (#341)

--- src/Components/Event/useEditForm.tsx
+++ src/Components/Event/useEditForm.tsx
@@ -96,6 +96,11 @@ export function useEditForm(event: Models.IEvent) {
   function setDescription(value = description) {
     let err: boolean = false;
 
+    if (type === EventType.Maintenance && !value) {
+      setValDescription("Description is required for maintenance.");
+      err = true;
+    }
+
     if (value && ([...value].length < 10 || [...value].length > 1500)) {
       setValDescription("Description must be between 10 and 1500 characters.");
       err = true;
